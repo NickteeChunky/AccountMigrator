7678e21c9ac4c63d3da18d8849a47cf876fac9f5
Nicktee Chunky <31430653+NickteeChunky@users.noreply.github.com>
2024-02-06 03:57:19 +0200

    Update CustomDatFilePlayerDataProvider.php
    oops phpstorm is weird

--- src/NickteeChunky/AccountMigrator/provider/CustomDatFilePlayerDataProvider.php
+++ src/NickteeChunky/AccountMigrator/provider/CustomDatFilePlayerDataProvider.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace AccountMigrator\src\NickteeChunky\AccountMigrator\provider;
+namespace NickteeChunky\AccountMigrator\provider;
 
 use pocketmine\errorhandler\ErrorToExceptionHandler;
 use pocketmine\nbt\BigEndianNbtSerializer;
